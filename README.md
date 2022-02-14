f507703769feb28e6d71eff252a4d3e992c76fc6
jogipr <jogiprashant@gmail.com>
2022-02-14 21:50:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-b1gcxt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-b1gcxt)
---
+++ script.js
@@ -0,0 +1,50 @@
+function creatSeats() {
+  let seats = [];
+  for (let i = 0; i <= 20; i++) {
+    seats.push({
+      id: i,
+      booked: false,
+    });
+  }
+  return seats;
+}
+let seats = creatSeats();
+
+function getSeatComponent({ id, booked }) {
+  const bookClass = booked ? 'booked' : 'unbooked';
+  return `<div id=${id} class='seat ${bookClass}'>${id}</div>`;
+}
+
+function drawSeats() {
+  const seatComponents = seats.map((seat) => getSeatComponent(seat)).join(' ');
+  const container = document.querySelector('.container');
+  container.innerHTML = seatComponents;
+}
+
+function bookTicket(event) {
+  const selectedSeat = event.target.id;
+  if(!selectedSeat) return;
+  seats = seats.map((seat) => {
+    if (selectedSeat == seat.id) {
+      return {
+        ...seat,
+        booked: !seat.booked,
+      };
+    }
+    return seat;
+  });
+  drawSeats();
+}
+
+function resetBooking() {
+  seats = creatSeats();
+  drawSeats();
+}
+
+window.addEventListener('load', drawSeats);
+
+const container = document.querySelector('.container');
+container.addEventListener('click', bookTicket);
+
+const resetButton = document.querySelector('#resetButton');
+resetButton.addEventListener('click', resetBooking);
